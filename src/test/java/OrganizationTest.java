c6f5eb10d82283930d4c5bb478b3a115cc430383
asxmas <asxmas@gmail.com>
2021-11-18 23:17:08 +0300

    final correction, add catalogs to gitignore

--- src/test/java/OrganizationTest.java
+++ src/test/java/OrganizationTest.java
@@ -11,11 +11,6 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
-//getLowestSalaryEmployees
-//getTopSalaryEmployees
-
-
 class OrganizationTest {
 
     private Organization organization;
